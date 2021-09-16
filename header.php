2ed9f9f7230020753a61b7aaa6aa0a0e0bf24467
DESKTOP-MUJTGOT\Ibn-e-Yunus <salmanabbas985@gmail.com>
2021-09-16 21:44:46 +0300

    header and footer added

---
+++ header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+</head>
+<body>
+
+    <header>
+        <nav class="bg-gray-800 py-8 px-8">
+            <a href="" class="text-white">Crud App</a>
+        </nav>
+    </header>
