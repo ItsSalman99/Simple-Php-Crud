b493a5d4bf992c0b01b404a6284051c4c8103b48
DESKTOP-MUJTGOT\Ibn-e-Yunus <salmanabbas985@gmail.com>
2021-09-16 21:45:21 +0300

    includes header and footer in index

--- index.php
+++ index.php
@@ -1,16 +1,11 @@
 <?php
-    require 'includes/db.inc.php';
+    require 'header.php';
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Index</title>
-</head>
-<body>
-    
-</body>
-</html>
+    <main>
+
+    </main>    
+
+<?php
+    require 'footer.php'
+?>
